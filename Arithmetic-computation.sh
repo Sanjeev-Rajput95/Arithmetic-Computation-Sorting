59491df7951ed77418aac9002f5f0cfe521e8589
sanjeev <sanjeevsingh.77d@gmail.com>
2022-09-21 17:02:00 +0530

    Dictionary has Stored in Array

--- Arithmetic-computation.sh
+++ Arithmetic-computation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 count=0;
 
@@ -8,21 +8,10 @@ read -p "Enter first number :" a;
 read -p "Enter second number :" b;
 read -p "Enter third number :" c;
 
-result1=$(( $a + $b * $c))
-echo "$a + $b * $c = $result1";
-compDictionary[1]=$result1;
+array[((count++))]=${compDictionary[1]}
+array[((count++))]=${compDictionary[2]}
+array[((count++))]=${compDictionary[3]}
+array[((count++))]=${compDictionary[4]}
 
-result2=$(( $a * $b + $c ))
-echo "$a * $b + $c = $result2";
-compDictionary[2]=$result2;
 
-result3=$(echo "scale=2; $c + $a / $b"|bc)
-echo "$c + $a / $b = $result3";
-compDictionary[3]=$result3;
-
-result4=$(( $a % $b + $c ))
-echo "$c % $b + $c = $result4";
-compDictionary[4]=$result4;
-
-echo "Result store in a Dictionary " ${compDictionary[@]}
-echo ${!compDictionary[@]}
+echo "Array is ${array[@]}"
