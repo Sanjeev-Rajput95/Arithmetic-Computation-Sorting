93003210ff31b260daf4a0e855bf40c905c97ce1
sanjeev <sanjeevsingh.77d@gmail.com>
2022-09-21 15:53:54 +0530

    MODIFIED : Store Result in Dictionary

--- Arithmetic-computation.sh
+++ Arithmetic-computation.sh
@@ -1,26 +1,28 @@
-#!/bin/bash
+#!/bin/bash -x
+
+count=0;
+
+declare -A compDictionary;
 
 read -p "Enter first number :" a;
 read -p "Enter second number :" b;
 read -p "Enter third number :" c;
 
-declare -A compDictionary;
-
 result1=$(( $a + $b * $c))
 echo "$a + $b * $c = $result1";
+compDictionary[1]=$result1;
 
 result2=$(( $a * $b + $c ))
 echo "$a * $b + $c = $result2";
+compDictionary[2]=$result2;
 
-result3=$(( $c + $a / $b ))
+result3=$(echo "scale=2; $c + $a / $b"|bc)
 echo "$c + $a / $b = $result3";
+compDictionary[3]=$result3;
 
 result4=$(( $a % $b + $c ))
 echo "$c % $b + $c = $result4";
+compDictionary[4]=$result4;
 
-compDictionary[result1]=$result1
-compDictionary[result2]=$result2
-compDictionary[result3]=$result3
-compDictionary[result4]=$result4
-
-echo "Results store in Dictionary " ${compDictionary[@]}
+echo "Result store in a Dictionary " ${compDictionary[@]}
+echo ${!compDictionary[@]}
